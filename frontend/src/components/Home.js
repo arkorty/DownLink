738d7528b78e180690ea6f7402509c34a865697a
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-08-14 21:40:58 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import DownloadForm from "./DownloadForm";
 import { TypeAnimation } from "react-type-animation";
@@ -7,19 +8,19 @@ const Home = () => {
   return (
     <div className="relative z-10 pt-20">
       <div className="max-w-4xl mx-auto text-center">
-        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl xl:text-8xl font-extrabold">
           <TypeAnimation
             sequence={[
               "From YouTube!",
-              1200,
-              "And Instagram",
-              1200,
+              1000,
+              "And Instagram!",
+              1000,
               "Download Now!",
-              1200,
+              1000,
               "HD Quality!",
-              1200,
+              1000,
               "DownLink!",
-              1200,
+              1000,
             ]}
             wrapper="span"
             speed={30}
